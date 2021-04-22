45e5e8d4317ea11db0fc1c658ecbcabfe01b4ae4
Nadiia Derevianchenko <nadiyaderevyanchenko@gmail.com>
2021-04-22 11:19:21 +0300

    ADDED | main theme files

---
+++ wp-content/themes/ashion/404.php
@@ -0,0 +1,17 @@
+<?php
+get_header();
+?>
+<section class="product spad">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-4">
+				<div class="section-title">
+					<h1><?php _e('Not found') ?></h1>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php
+get_footer();
