47e823e893980ecbc5c7e79e6afd319dd7132771
Nadiia Derevianchenko <nadiyaderevyanchenko@gmail.com>
2021-04-22 11:22:37 +0300

    UPDATED | small fixes

--- wp-content/themes/ashion/functions.php
+++ wp-content/themes/ashion/functions.php
@@ -2,6 +2,9 @@
 define('THEME_DIR', get_template_directory());
 define('THEME_DIR_URI', get_template_directory_uri());
 
+//temporary variable
+$temp_html = THEME_DIR_URI . '/shop/'; //TODO delete before production
+
 require_once THEME_DIR .'/inc/helpers/file.php';
 require_once THEME_DIR .'/inc/wp/enqueue_scripts.php';
 require_once THEME_DIR .'/inc/wp/theme-support.php';
