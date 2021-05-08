c70f7a975ff843412a35276e61422172772e0e39
Nadiia Derevianchenko <nadiyaderevyanchenko@gmail.com>
2021-05-08 08:15:54 +0300

    UPDATED | Search Template

--- wp-content/themes/ashion/search.php
+++ wp-content/themes/ashion/search.php
@@ -1,2 +1,25 @@
 <?php
-echo 'search.php';
+get_header(); ?>
+
+<?php echo get_breadcrumbs_section(); ?>
+
+<section class="blog spad">
+	<div class="container">
+		<div class="row">
+			<?php
+			if (have_posts()) {
+				while (have_posts()) {
+					the_post();
+					get_template_part('template-parts/posts/post', 'item');
+				}
+				//TODO pagination
+			} else {
+				//TODO no-content template-part?
+				echo 'No Results';
+			}
+			?>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
