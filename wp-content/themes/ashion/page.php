ecff706daffb6fb45c302cb48d105670ef27d85d
Nadiia Derevianchenko <nadiyaderevyanchenko@gmail.com>
2021-05-08 08:14:05 +0300

    ADDED | Page Template

--- wp-content/themes/ashion/page.php
+++ wp-content/themes/ashion/page.php
@@ -1,2 +1,100 @@
 <?php
-echo 'page.php';
+get_header();
+
+$title = get_the_title();
+$content = get_the_content();
+$thumbnail = has_post_thumbnail() ? get_the_post_thumbnail('', 'large') : '<img src="'.get_template_directory_uri().'/dist/images/default-placeholder.png" />';
+$date = get_the_date('M d, Y');
+$author = get_the_author();
+?>
+
+<?php echo get_breadcrumbs_section(); ?>
+
+<!-- Contact Section Begin -->
+<section class="page spad">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-md-8">
+				<div class="blog__details__content">
+					<div class="blog__details__item">
+						<?php echo $thumbnail; ?>
+						<div class="blog__details__item__title">
+							<h4>
+								<?php echo $title; ?>
+							</h4>
+							<ul>
+								<li><?php _e('by', 'ashion');?> <span><?php echo $author; ?></span></li>
+								<li><?php echo $date; ?></li>
+							</ul>
+						</div>
+					</div>
+					<div class="blog__details__desc">
+						<?php echo $content; ?>
+					</div>
+				</div>
+			</div>
+			<?php get_sidebar(); ?>
+		</div>
+	</div>
+</section>
+<!-- Contact Section End -->
+
+<!-- Instagram Begin -->
+<div class="instagram">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-2 col-md-4 col-sm-4 p-0">
+				<div class="instagram__item set-bg" data-setbg="img/instagram/insta-1.jpg">
+					<div class="instagram__text">
+						<i class="fa fa-instagram"></i>
+						<a href="#">@ ashion_shop</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-4 p-0">
+				<div class="instagram__item set-bg" data-setbg="img/instagram/insta-2.jpg">
+					<div class="instagram__text">
+						<i class="fa fa-instagram"></i>
+						<a href="#">@ ashion_shop</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-4 p-0">
+				<div class="instagram__item set-bg" data-setbg="img/instagram/insta-3.jpg">
+					<div class="instagram__text">
+						<i class="fa fa-instagram"></i>
+						<a href="#">@ ashion_shop</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-4 p-0">
+				<div class="instagram__item set-bg" data-setbg="img/instagram/insta-4.jpg">
+					<div class="instagram__text">
+						<i class="fa fa-instagram"></i>
+						<a href="#">@ ashion_shop</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-4 p-0">
+				<div class="instagram__item set-bg" data-setbg="img/instagram/insta-5.jpg">
+					<div class="instagram__text">
+						<i class="fa fa-instagram"></i>
+						<a href="#">@ ashion_shop</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-4 p-0">
+				<div class="instagram__item set-bg" data-setbg="img/instagram/insta-6.jpg">
+					<div class="instagram__text">
+						<i class="fa fa-instagram"></i>
+						<a href="#">@ ashion_shop</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Instagram End -->
+
+
+<?php get_footer(); ?>
