f443b59970d1e402e0db054e89f975693766dfea
Nadiia Derevianchenko <nadiyaderevyanchenko@gmail.com>
2021-05-08 08:11:23 +0300

    UPDATED | 404.php

--- wp-content/themes/ashion/404.php
+++ wp-content/themes/ashion/404.php
@@ -1,12 +1,12 @@
 <?php
 get_header();
 ?>
-<section class="product spad">
+<section class="blog-details spad">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-4 col-md-4">
 				<div class="section-title">
-					<h1><?php _e('Not found') ?></h1>
+					<h1><?php _e('Page not found') ?></h1>
 				</div>
 			</div>
 		</div>
