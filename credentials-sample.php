e3902d59b652fdc314d4bffe997410dae4b08fa2
Nadiia Derevianchenko <nadiyaderevyanchenko@gmail.com>
2021-05-18 16:24:35 +0300

    ADDED | debug settings

--- credentials-sample.php
+++ credentials-sample.php
@@ -1,16 +1,18 @@
 <?php
 
 $cred = array(
-	'dev' => array(
+	'dev'  => array(
 		'db_name'     => '',
 		'db_user'     => '',
 		'db_password' => '',
 		'db_host'     => '',
+		'debug'       => 'true',
 	),
 	'prod' => array(
 		'db_name'     => '',
 		'db_user'     => '',
 		'db_password' => '',
 		'db_host'     => '',
+		'debug'       => 'false',
 	)
 );
